51be2ed5e87cebbc8421014bf57e9ecf970dbaed
skyjake <skyjake@3894b877-cc0d-0410-b400-92b7ed5dc389>
2003-09-02 22:13:04 +0000

    Fixed to work with new directory setup

--- distrib/snap.py
+++ distrib/snap.py
@@ -6,7 +6,9 @@
 # a RAR archive is created.
 #
 # Options:
+# -nogame		Exclude game DLLs
 # -nodll		Exclude non-game DLLs
+# -norend		Exclude rendering DLLs
 # -nosnd		Exclude sound DLLs
 # -ogl			Exclude Direct3D
 # -def			Include definition files
@@ -18,6 +20,8 @@ import os, tempfile, sys, shutil, re
 baseDir = 'C:/Projects/de1.7/doomsday'
 outDir  = 'C:/Projects/de1.7/distrib/out'
 
+print "Source directory: " + baseDir
+
 # Create the temporary directory.
 archDir = tempfile.mktemp()
 print "Using temporary directory: " + archDir
@@ -28,19 +32,24 @@ if '-def' in sys.argv:
 for sub in neededDirs:
 	os.mkdir( os.path.join( archDir, sub ) )
 
-files = [ ('Bin/Release/Doomsday.exe', 'Bin/Doomsday.exe'),
-          ('Bin/Release/jDoom.dll', 'Bin/jDoom.dll'),
-          ('Bin/Release/jHeretic.dll', 'Bin/jHeretic.dll'),
-          ('Bin/Release/jHexen.dll', 'Bin/jHexen.dll') ]
+files = [ ('Bin/Release/Doomsday.exe', 'Bin/Doomsday.exe') ]
+
+if not '-nogame' in sys.argv:
+	files += [ ('Bin/Release/jDoom.dll', 'Bin/jDoom.dll'),
+               ('Bin/Release/jHeretic.dll', 'Bin/jHeretic.dll'),
+               ('Bin/Release/jHexen.dll', 'Bin/jHexen.dll') ]
+
+includeRender = not '-norend' in sys.argv
 
 # Select the appropriate files.
 if not '-nodll' in sys.argv:
-	files.append( ('Bin/Release/drOpenGL.dll', 'Bin/drOpenGL.dll') )
+	if includeRender:
+		files.append( ('Bin/Release/drOpenGL.dll', 'Bin/drOpenGL.dll') )
 	files.append( ('Bin/Release/dpDehRead.dll', 'Bin/dpDehRead.dll') )
 	if not '-nosnd' in sys.argv:
 		files += [ ('Bin/Release/dsA3D.dll', 'Bin/dsA3D.dll'),
 		           ('Bin/Release/dsCompat.dll', 'Bin/dsCompat.dll') ]
-	if not '-ogl' in sys.argv:
+	if not '-ogl' in sys.argv and includeRender:
 		files.append( ('Bin/Release/drD3D.dll', 'Bin/drD3D.dll') )
 
 # These defs will be included, if present.
@@ -76,7 +85,11 @@ for src, dest in files:
 
 # Make the archive.
 outFile = os.path.join( outDir, 'ddsnapshot.rar' )
-os.remove( outFile )
+try:
+	os.remove( outFile )
+except:
+	# Never mind.
+	pass
 os.system( "rar -r -ep1 -m5 a %s %s\\*" % (outFile, archDir) )
 
 print "Removing temporary directory: " + archDir
@@ -85,9 +98,9 @@ shutil.rmtree( archDir )
 if not '-test' in sys.argv:
 	# Upload.
 	print "Uploading to the Mirror..."
-	os.system( "ftpscrpt -f z:\scripts\mirror-snapshot.scp" )
+	os.system( "ftpscrpt -f c:\projects\doomsday\scripts\mirror-snapshot.scp" )
 	print "Uploading to Fourwinds..."
-	os.system( "ftpscrpt -f z:\scripts\snapshot.scp" )
+	os.system( "ftpscrpt -f c:\projects\doomsday\scripts\snapshot.scp" )
 else:
 	print "--- THIS WAS JUST A TEST ---"
 
