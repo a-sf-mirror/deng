45f571ca19464ce8488c01453385730cafae4154
skyjake <skyjake@3894b877-cc0d-0410-b400-92b7ed5dc389>
2003-10-09 16:29:55 +0000

    Version upgrade to 1.7.13 + patch9

--- doomsday/Include/dd_share.h
+++ doomsday/Include/dd_share.h
@@ -44,7 +44,7 @@ Version number rules: (major).(minor).(revision)
 // Version constant. Use this to verify in the DLL that the engine
 // is new enough. 
 #define DOOMSDAY_VERSION		10713
-#define DOOMSDAY_VERSION_TEXT	"1.7.13-8"
+#define DOOMSDAY_VERSION_TEXT	"1.7.13-9"
 
 #define DDMAXPLAYERS			16
 
