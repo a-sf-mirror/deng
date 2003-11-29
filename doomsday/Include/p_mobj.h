bb8b221c15a6d84e7e0bac084ac540db0fb348a8
skyjake <skyjake@3894b877-cc0d-0410-b400-92b7ed5dc389>
2003-11-29 21:58:07 +0000

    Renamed some of the playsim functions to avoid conflicts

--- doomsday/Include/p_mobj.h
+++ doomsday/Include/p_mobj.h
@@ -17,13 +17,13 @@ extern boolean dontHitMobjs;
 #include "cl_def.h" // for playerstate_s
 
 void P_SetState(mobj_t *mo, int statenum);
-void P_XYMovement(mobj_t* mo);
-void P_XYMovement2(mobj_t* mo, void *pstate);
-void P_ZMovement(mobj_t* mo);
-boolean P_TryMove(mobj_t* thing, fixed_t x, fixed_t y, fixed_t z);
+void P_ThingMovement(mobj_t* mo);
+void P_ThingMovement2(mobj_t* mo, void *pstate);
+void P_ThingZMovement(mobj_t* mo);
+boolean P_TryMoveXYZ(mobj_t* thing, fixed_t x, fixed_t y, fixed_t z);
 boolean P_StepMove(mobj_t *thing, fixed_t dx, fixed_t dy, fixed_t dz);
-boolean P_CheckPosition(mobj_t *thing, fixed_t x, fixed_t y);
-boolean P_CheckPosition2(mobj_t* thing, fixed_t x, fixed_t y, fixed_t z);
+boolean P_CheckPosXY(mobj_t *thing, fixed_t x, fixed_t y);
+boolean P_CheckPosXYZ(mobj_t* thing, fixed_t x, fixed_t y, fixed_t z);
 boolean P_ChangeSector(sector_t *sector);
 
 #endif
