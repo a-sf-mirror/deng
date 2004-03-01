e909d7e8a57f36a80cf17a8fd3e44597d24da00b
(no author) <(no author)@3894b877-cc0d-0410-b400-92b7ed5dc389>
2004-03-01 18:20:33 +0000

    This commit was manufactured by cvs2svn to create branch 'jaakko'.

--- doomsday/Include/Common/g_update.h
+++
@@ -1,7 +0,0 @@
-#ifndef __GAME_UPDATE_H__
-#define __GAME_UPDATE_H__
-
-void G_SetGlowing(void);
-void G_UpdateState(int step);
-
-#endif
--- doomsday/Include/Common/hu_pspr.h
+++
@@ -1,6 +0,0 @@
-#ifndef __COMMON_PSPRITE_H__
-#define __COMMON_PSPRITE_H__
-
-void HU_UpdatePsprites(void);
-
-#endif
