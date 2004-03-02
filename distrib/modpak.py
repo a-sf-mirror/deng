696525c1c95427f83d524c6442bfccebd15ca1ce
skyjake <skyjake@3894b877-cc0d-0410-b400-92b7ed5dc389>
2004-03-02 13:17:09 +0000

    Works in Linux, builds Collections and Single packages with -final

--- distrib/modpak.py
+++ distrib/modpak.py
@@ -1,17 +1,20 @@
+#!/usr/bin/python
 # Doomsday Project Model Pack Builder
 # $Id$
 #
 # This script will build both the individual object packs and
 # the collection packs.
 #
-# -nofinal: Don't create distribution ZIPs
+# -final: Create the final distribution ZIPs
 
 import os, sys, re, string
 
+# Remove the file if it exists.
 def obsolete( fileName ):
 	if os.path.exists( fileName ):
 		os.remove( fileName )
 
+# Build a list of package names.
 def build_list( top, path = None ):
 	files = []
 	for f in os.listdir( top ):
@@ -33,8 +36,34 @@ def build_list( top, path = None ):
 				pass
 	return files
 
-baseDir  = 'C:/Projects/de1.7/doomsday'
-outDir   = 'C:/Projects/de1.7/distrib/Out/Models'
+# Add a new member to a group.
+def collection_add( collec, group, member ):
+	for i in collec:
+		if i[0] == group:
+			i[1].append( member )
+			return
+	collec.append( (group, [member]) )
+
+# Create a zip archive.
+def run_zip( dest, files, options ):
+	os.system( string.join( ['zip', options, dest, files] ) )
+
+def zip_store( dest, files, options = '', exclude = '' ):
+	exOpt = ''
+	if exclude != '':
+		exOpt = "-x '" + exclude + "'"
+	run_zip( dest, string.join( [files, exOpt] ), '-0 ' + options )
+
+def zip_max( dest, files, options = '' ):
+	run_zip( dest, files, '-9 ' + options )
+
+
+# Operating mode.
+distroMode = '-final' in sys.argv
+
+# Directory configuration:
+baseDir  = os.path.abspath( '../doomsday' )
+outDir   = os.path.abspath( 'Out/Models' )
 finalDir = os.path.join( outDir, 'Final' )
 origDir  = os.getcwd()
 
@@ -57,29 +86,40 @@ else:
 	print "Unknown game."
 	sys.exit()
 
-# First build the full pack by adding all the models.
-os.chdir( baseDir )
-outFile = os.path.join( outDir, output + '.pk3' )
-obsolete( outFile )
-os.system( 'wzzip ' + outFile + \
-	' -e0 -r -P -x*\\CVS\\*.* Defs\\' + game + '\\Models\\*.* ' + \
-	' -x*.exe -x*.bat ' + \
-	'Data\\' + game + '\\Models\\*.* ' + \
-	'Defs\\' + game + '\\Models.ded' )
+# Model directory configuration:
+defsDir = os.path.join( 'Defs', game )
+dataDir = os.path.join( 'Data', game )
+docDir  = os.path.join( 'Doc', game )
+modelDefsDir = os.path.join( defsDir, 'Models' )
+modelDataDir = os.path.join( dataDir, 'Models' )
 
-# Build the Common pack.
-cmFile = os.path.join( outDir, prefix + 'Common.pk3' )
-obsolete( cmFile )
-os.system( 'wzzip ' + cmFile + \
-	' -e0 -r -P -x*\\CVS\\*.* Defs\\' + game + '\\Models\\Common\\*.* ' + \
-	' -x*.exe -x*.bat ' + \
-	'Data\\' + game + '\\Models\\Common\\*.* ' + \
-	'Defs\\' + game + '\\Models.ded' )
+os.chdir( baseDir )
+
+if not distroMode:
+
+	# First build the full pack by adding all the models.
+	outFile = os.path.join( outDir, output + '.pk3' )
+	obsolete( outFile )
+	zip_store( outFile, modelDefsDir, '-r', '*/CVS/*' )
+	zip_store( outFile, os.path.join( defsDir, 'Models.ded' ) )
+	zip_store( outFile, modelDataDir, '-r', '*.bat' )
+	zip_store( outFile, os.path.join( docDir, 'Models.txt' ) )
+
+	# Build the Common pack.
+	cmFile = os.path.join( outDir, prefix + 'Common.pk3' )
+	obsolete( cmFile )
+	zip_store( cmFile, os.path.join( modelDefsDir, 'Common' ), '-r', '*/CVS/*' )
+	zip_store( cmFile, os.path.join( modelDataDir, 'Common' ), '-r' )
+	zip_store( cmFile, os.path.join( defsDir, 'Models.ded' ) )
+
+# We'll build a list of packages for each main class. One collection ZIP
+# will be compiled for each class.
+collections = []
 
 # Build a separate pack for each object (i.e. for each DED file).
-for ded in build_list( os.path.join( baseDir, 'Defs\\' + game + '\\Models' ) ):
+for ded in build_list( os.path.join( baseDir, modelDefsDir ) ):
 	# Compose the pack name.
-	parts = ded.split( '\\' )
+	parts = ded.split( '/' )
 	# Start with the prefix.
 	modName = prefix
 	# The first component is the main object class.
@@ -101,17 +141,25 @@ for ded in build_list( os.path.join( baseDir, 'Defs\\' + game + '\\Models' ) ):
 	# Create the PK3.
 	mFile = os.path.join( outDir, modName + '.pk3' )
 	print "--> " + mFile
-	obsolete( mFile )
-	os.system( 'wzzip ' + mFile +
-		' -e0 -r -P -x*\\CVS\\*.* Defs\\' + modPath + '.ded ' + \
-		'Data\\' + modPath + '\\*.* ' + \
-		'Defs\\' + game + '\\Models.ded' )
 
+	if not distroMode:
+		obsolete( mFile )
+		zip_store( mFile, os.path.join( 'Defs', modPath + '.ded' ) )
+		zip_store( mFile, os.path.join( 'Data', modPath ), '-r' )
+		zip_store( mFile, os.path.join( defsDir, 'Models.ded' ) )
+		zip_store( mFile, os.path.join( docDir, 'Models.txt' ) )
+
+	# Add to the class collection.
+	collection_add( collections, prefix + parts[0], mFile )
+
+# Include the Common package in all collections.
+for collec in collections:
+	collec[1].append( os.path.join( outDir, prefix + 'Common.pk3' ) )
 
 os.chdir( origDir )
 print "Returning to: " + origDir
 
-if not '-nofinal' in sys.argv:
+if distroMode:
 	# Create a final compressed ZIP file out of each PK3.
 	for file in os.listdir( outDir ):
 
@@ -121,11 +169,35 @@ if not '-nofinal' in sys.argv:
 
 		# Which game is it?
 		fileGame = "jHeretic"
-		if file[0] == 'X': fileGame = "jHexen"
+		if file[0] == 'X' or file[:2] == 'jX': fileGame = "jHexen"
+		if file[0] == 'j':
+			destDir = finalDir
+		else:
+			destDir = os.path.join( finalDir, 'Single' )
+		if fileGame != game: continue
 
 		# Pack into a compressed ZIP.
-		packFile = os.path.join( finalDir, found.group(1) + '.zip' )
+		packFile = os.path.join( destDir, found.group(1) + '.zip' )
 		print "Packing full pack to: " + packFile
 		obsolete( packFile )
-		os.system( 'wzzip ' + packFile + ' -ex ' + \
-			os.path.join( outDir, file ) + ' Include\\' + fileGame + '\\Readme.txt' )
+		zip_max( packFile, string.join( [\
+			os.path.join( outDir, file ), \
+			os.path.join( 'Include', fileGame, 'Readme.txt' ), \
+			os.path.join( baseDir, docDir, 'Models.txt' ) ] ), '-j' )
+
+	# Build collections.
+	for collec in collections:
+
+		# Which game is it?
+		fileGame = "jHeretic"
+		if collec[0][0] == 'X': fileGame = "jHexen"
+		if fileGame != game: continue
+
+		# Create a compressed ZIP with multiple PK3s.
+		packFile = os.path.join( finalDir, 'Collections', collec[0] + '.zip' )
+		print "Packing collection: " + packFile
+		obsolete( packFile )
+		zip_max( packFile, string.join( collec[1] ), '-j' )
+		zip_max( packFile, string.join( [
+			os.path.join( 'Include', fileGame, 'Readme.txt' ), \
+			os.path.join( baseDir, docDir, 'Models.txt' ) ] ), '-j' )
