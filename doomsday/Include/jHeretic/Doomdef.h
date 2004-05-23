71ee1ad4161d540fb1b26ef3bbaad375f76d9d98
skyjake <skyjake@3894b877-cc0d-0410-b400-92b7ed5dc389>
2004-05-23 14:34:08 +0000

    New version information

--- doomsday/Include/jHeretic/Doomdef.h
+++ doomsday/Include/jHeretic/Doomdef.h
@@ -7,25 +7,11 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "version.h"
+
 #ifdef WIN32
 #pragma warning (disable:4244 4761)
 #endif
-
-//#define VERSION 130 // Original Heretic version.
-
-#define SAVE_VERSION	130	// Don't change this.
-
-#ifndef JHERETIC_VER_ID
-#ifdef _DEBUG
-#define JHERETIC_VER_ID "+D Doomsday"
-#else
-#define JHERETIC_VER_ID "Doomsday"
-#endif
-#endif
-
-#define VERSION 210
-#define VERSION_TEXT "1.3.10"
-#define VERSIONTEXT "Version "VERSION_TEXT" "__DATE__" ("JHERETIC_VER_ID")"
 
 #include "../doomsday.h"
 #include "../dd_api.h"
@@ -104,7 +90,7 @@ typedef enum
 	sk_nightmare
 } skill_t;
 
-typedef struct
+/*typedef struct
 {
 	char		forwardmove;		// *2048 for move
 	char		sidemove;			// *2048 for move
@@ -113,7 +99,7 @@ typedef struct
 	byte		buttons;
 	byte		lookfly;			// look/fly up/down/centering
 	byte		arti;				// artitype_t to use
-} ticcmd_t;
+} ticcmd_t;*/
 
 #define	BT_ATTACK		1
 #define	BT_USE			2
