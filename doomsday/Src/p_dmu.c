0da50d4d364a1386be875a9c4d7f382a6b0a0714
skyjake <skyjake@3894b877-cc0d-0410-b400-92b7ed5dc389>
2006-01-21 12:35:53 +0000

    Added new source and header files for DMU and DAM

---
+++ doomsday/Src/p_dmu.c
@@ -0,0 +1,46 @@
+/* DE1: $Id$
+ * Copyright (C) 2006 Jaakko Keränen <jaakko.keranen@iki.fi>
+ *                    Daniel Swanson <danij@users.sourceforge.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not: http://www.opensource.org/
+ */
+
+/*
+ * p_dmu.c: Doomsday Map Update API
+ *
+ * The Map Update API is used for accessing and making changes to map data 
+ * during gameplay. From here, the relevant engine's subsystems will be 
+ * notified of changes in the map data they use, thus allowing them to 
+ * update their status whenever needed.
+ */
+
+// HEADER FILES ------------------------------------------------------------
+
+// MACROS ------------------------------------------------------------------
+
+// TYPES -------------------------------------------------------------------
+
+// EXTERNAL FUNCTION PROTOTYPES --------------------------------------------
+
+// PUBLIC FUNCTION PROTOTYPES ----------------------------------------------
+
+// PRIVATE FUNCTION PROTOTYPES ---------------------------------------------
+
+// EXTERNAL DATA DECLARATIONS ----------------------------------------------
+
+// PUBLIC DATA DEFINITIONS -------------------------------------------------
+
+// PRIVATE DATA DEFINITIONS ------------------------------------------------
+
+// CODE --------------------------------------------------------------------
