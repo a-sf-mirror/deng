00b2d4635cd92f0a841a2739c3d1b1529064736a
danij <danij@dengine.net>
2010-02-03 00:42:11 +0000

    Imported binary angle math from doomsday into libdeng2.

--- doomsday/libdeng2/include/de/math.h
+++ doomsday/libdeng2/include/de/math.h
@@ -1,7 +1,8 @@
 /*
  * The Doomsday Engine Project -- libdeng2
  *
- * Copyright (c) 2004-2009 Jaakko Keränen <jaakko.keranen@iki.fi>
+ * Copyright © 2004-2010 Jaakko Keränen <jaakko.keranen@iki.fi>
+ * Copyright © 2005-2010 Daniel Swanson <danij@dengine.net>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -60,17 +61,17 @@ namespace de
     inline const Type& max(const Type& a, const Type& b) {
         return (a > b? a : b);
     }
-    
+
     /// Clamp value within range.
     template <typename Type>
     inline Type clamp(const Type& low, const Type& value, const Type& high) {
         return min(max(value, low), high);
     }
-    
+
     /// Compare two floating-point values for equality, with the precision of EPSILON.
     inline ddouble fequal(ddouble a, ddouble b) {
         return abs(a - b) < EPSILON;
-    }    
+    }
 
     /// Return the first 32-bit integer larger than the specified value.
     template <typename Type>
@@ -87,6 +88,84 @@ namespace de
         if(a > b) return 1;
         return 0;
     }
+
+    /**
+     * Binary Angle Mathematics (BAMS).
+     */
+#if LIBDENG2_BAMS_BITS == 32
+    typedef duint32 binangle_t;
+#elif LIBDENG2_BAMS_BITS == 16
+    typedef duint16 binangle_t;
+#else
+    typedef duint8 binangle_t;
+#endif
+
+    /// Some predefined angles.
+#if LIBDENG2_BAMS_BITS == 32
+    static const binangle_t BANG_0   = 0; // East.
+    static const binangle_t BANG_45  = 0x20000000; // Northeast.
+    static const binangle_t BANG_90  = 0x40000000; // North.
+    static const binangle_t BANG_135 = 0x60000000; // Northwest.
+    static const binangle_t BANG_180 = 0x80000000; // West.
+    static const binangle_t BANG_225 = 0xa0000000; // Southwest.
+    static const binangle_t BANG_270 = 0xc0000000; // South.
+    static const binangle_t BANG_315 = 0xe0000000; // Southeast.
+    static const binangle_t BANG_360 = BANG_0;
+    static const binangle_t BANG_MAX = 0xffffffff; // Largest representable value.
+#elif LIBDENG2_BAMS_BITS == 16
+    static const binangle_t BANG_0   = 0; // East.
+    static const binangle_t BANG_45  = 0x2000; // Northeast.
+    static const binangle_t BANG_90  = 0x4000; // North.
+    static const binangle_t BANG_135 = 0x6000; // Northwest.
+    static const binangle_t BANG_180 = 0x8000; // West.
+    static const binangle_t BANG_225 = 0xa000; // Southwest.
+    static const binangle_t BANG_270 = 0xc000; // South.
+    static const binangle_t BANG_315 = 0xe000; // Southeast.
+    static const binangle_t BANG_360 = BANG_0;
+    static const binangle_t BANG_MAX = 0xffff; // Largest representable value.
+#else // Byte-sized.
+    static const binangle_t BANG_0   = 0; // East.
+    static const binangle_t BANG_45  = 0x20; // Northeast.
+    static const binangle_t BANG_90  = 0x40; // North.
+    static const binangle_t BANG_135 = 0x60; // Northwest.
+    static const binangle_t BANG_180 = 0x80; // West.
+    static const binangle_t BANG_225 = 0xa0; // Southwest.
+    static const binangle_t BANG_270 = 0xc0; // South.
+    static const binangle_t BANG_315 = 0xe0; // Southeast.
+    static const binangle_t BANG_360 = BANG_0;
+    static const binangle_t BANG_MAX = 0xff; // Largest representable value.
+#endif
+
+    /// Compass directions, for convenience.
+    static const binangle_t BANG_EAST      = BANG_0;
+    static const binangle_t BANG_NORTHEAST = BANG_45;
+    static const binangle_t BANG_NORTH     = BANG_90;
+    static const binangle_t BANG_NORTHWEST = BANG_135;
+    static const binangle_t BANG_WEST      = BANG_180;
+    static const binangle_t BANG_SOUTHWEST = BANG_225;
+    static const binangle_t BANG_SOUTH     = BANG_270;
+    static const binangle_t BANG_SOUTHEAST = BANG_315;
+
+    static const dfloat BAMS_PI = 3.14159265359f;
+
+    /// Radians to binary angle.
+    template <typename Type>
+    inline binangle_t rad2bang(const Type& rad) {
+        return static_cast<binangle_t>(rad / BAMS_PI * BANG_180);
+    }
+
+    /// Binary angle to radians.
+    inline dfloat bang2rad(binangle_t bang) {
+        return static_cast<dfloat>((bang / (dfloat) BANG_180 * BAMS_PI));
+    }
+
+    /// Binary angle to degrees.
+    inline dfloat bang2deg(binangle_t bang) {
+        return static_cast<dfloat>(bang / (dfloat) BANG_180 * 180);
+    }
+
+    void            InitBAMLUTs(void); // Fill in the tables.
+    binangle_t      bamsAtan2(dint y, dint x);
 }
 
 #endif /* LIBDENG2_MATH_H */
