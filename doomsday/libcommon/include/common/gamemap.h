54881e6c3325865ddb51863a3640488453ebbbc6
danij <danij@dengine.net>
2010-03-13 19:36:19 +0000

    Moved members of the temporary gamemap_t into GameMap. Removed gamemap_t

--- doomsday/libcommon/include/common/gamemap.h
+++ doomsday/libcommon/include/common/gamemap.h
@@ -1,7 +1,8 @@
 /*
  * The Doomsday Engine Project
  *
- * Copyright (c) 2009 Jaakko Keränen <jaakko.keranen@iki.fi>
+ * Copyright © 2003-2010 Jaakko Keränen <jaakko.keranen@iki.fi>
+ * Copyright © 2005-2010 Daniel Swanson <danij@dengine.net>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -24,18 +25,544 @@
 
 #include <de/Map>
 
+#include "p_iterlist.h"
+#if __JDOOM__ || __JDOOM64__ || __JHERETIC__
+#  include "p_xg.h"
+#endif
+
+/**
+ * @defGroup mapSpotFlags Map Spot Flags
+ * @todo Commonize these flags and introduce translations where needed.
+ */
+/*{*/
+#define MSF_EASY            0x00000001 // Appears in easy skill modes.
+#define MSF_MEDIUM          0x00000002 // Appears in medium skill modes.
+#define MSF_HARD            0x00000004 // Appears in hard skill modes.
+
+#if __JDOOM__ || __JDOOM64__
+# define MSF_DEAF           0x00000008 // Thing is deaf.
+#elif __JHERETIC__ || __JHEXEN__
+# define MSF_AMBUSH         0x00000008 // Mobj will be deaf spawned deaf.
+#endif
+
+#if __JDOOM__ || __JDOOM64__ || __JHERETIC__
+# define MSF_NOTSINGLE      0x00000010 // Appears in multiplayer game modes only.
+#elif __JHEXEN__
+# define MTF_DORMANT        0x00000010
+#endif
+
+#if __JDOOM__ || __JHERETIC__
+# define MSF_NOTDM          0x00000020 // (BOOM) Can not be spawned in the Deathmatch gameMode.
+# define MSF_NOTCOOP        0x00000040 // (BOOM) Can not be spawned in the Co-op gameMode.
+# define MSF_FRIENDLY       0x00000080 // (BOOM) friendly monster.
+#elif __JDOOM64__
+# define MSF_DONTSPAWNATSTART 0x00000020 // Do not spawn this thing at map start.
+# define MSF_SCRIPT_TOUCH   0x00000040 // Mobjs spawned from this spot will envoke a script when touched.
+# define MSF_SCRIPT_DEATH   0x00000080 // Mobjs spawned from this spot will envoke a script on death.
+# define MSF_SECRET         0x00000100 // A secret (bonus) item.
+# define MSF_NOTARGET       0x00000200 // Mobjs spawned from this spot will not target their attacker when hurt.
+# define MSF_NOTDM          0x00000400 // Can not be spawned in the Deathmatch gameMode.
+# define MSF_NOTCOOP        0x00000800 // Can not be spawned in the Co-op gameMode.
+#elif __JHEXEN__
+# define MSF_FIGHTER        0x00000020
+# define MSF_CLERIC         0x00000040
+# define MSF_MAGE           0x00000080
+# define MSF_NOTSINGLE      0x00000100
+# define MSF_NOTCOOP        0x00000200
+# define MSF_NOTDM          0x00000400
+// The following are not currently implemented.
+# define MSF_SHADOW         0x00000800 // (ZDOOM) Thing is 25% translucent.
+# define MSF_INVISIBLE      0x00001000 // (ZDOOM) Makes the thing invisible.
+# define MSF_FRIENDLY       0x00002000 // (ZDOOM) Friendly monster.
+# define MSF_STILL          0x00004000 // (ZDOOM) Thing stands still (only useful for specific Strife monsters or friendlies).
+#endif
+
+// New flags:
+#define MSF_Z_FLOOR         0x20000000 // Spawn relative to floor height.
+#define MSF_Z_CEIL          0x40000000 // Spawn relative to ceiling height (minus thing height).
+#define MSF_Z_RANDOM        0x80000000 // Random point between floor and ceiling.
+
+// Unknown flag mask:
+#if __JDOOM__
+#define MASK_UNKNOWN_MSF_FLAGS (0xffffffff \
+    ^ (MSF_EASY|MSF_MEDIUM|MSF_HARD|MSF_DEAF|MSF_NOTSINGLE|MSF_NOTDM|MSF_NOTCOOP|MSF_FRIENDLY))
+#elif __JDOOM64__
+#define MASK_UNKNOWN_MSF_FLAGS (0xffffffff \
+    ^ (MSF_EASY|MSF_MEDIUM|MSF_HARD|MSF_DEAF|MSF_NOTSINGLE|MSF_DONTSPAWNATSTART|MSF_SCRIPT_TOUCH|MSF_SCRIPT_DEATH|MSF_SECRET|MSF_NOTARGET|MSF_NOTDM|MSF_NOTCOOP))
+#elif __JHERETIC__
+#define MASK_UNKNOWN_MSF_FLAGS (0xffffffff \
+    ^ (MSF_EASY|MSF_MEDIUM|MSF_HARD|MSF_AMBUSH|MSF_NOTSINGLE|MSF_NOTDM|MSF_NOTCOOP|MSF_FRIENDLY))
+#elif __JHEXEN__
+#define MASK_UNKNOWN_MSF_FLAGS (0xffffffff \
+    ^ (MSF_EASY|MTF_NORMAL|MSF_HARD|MSF_AMBUSH|MTF_DORMANT|MSF_FIGHTER|MSF_CLERIC|MSF_MAGE|MSF_GSINGLE|MSF_GCOOP|MSF_GDEATHMATCH|MSF_SHADOW|MSF_INVISIBLE|MSF_FRIENDLY|MSF_STILL))
+#endif
+/*}*/
+
+typedef struct {
+#if __JHEXEN__
+    dshort          tid;
+#endif
+    dfloat          pos[3];
+    dangle          angle;
+    dint            doomEdNum;
+    dint            flags;
+#if __JHEXEN__
+    dbyte           special;
+    dbyte           arg1;
+    dbyte           arg2;
+    dbyte           arg3;
+    dbyte           arg4;
+    dbyte           arg5;
+#endif
+} mapspot_t;
+
+typedef struct {
+    dint            plrNum;
+    dbyte           entryPoint;
+    dfloat          pos[3];
+    dangle          angle;
+    dint            spawnFlags; // @see mapSpotFlags
+} playerstart_t;
+
+typedef struct taglist_s {
+    dint            tag;
+    iterlist_t*     list;
+} taglist_t;
+
+#define BODYQUEUESIZE           32
+
+#if __JHEXEN__
+#define CORPSEQUEUESIZE         64
+#endif
+
+#if __JHEXEN__
+#define MAX_TID_COUNT           200
+#endif
+
+#if __JHEXEN__
+#define STAIR_QUEUE_SIZE        32
+
+typedef struct stairqueue_s {
+    Sector*         sector;
+    dint            type;
+    dfloat          height;
+} stairqueue_t;
+
+// Global vars for stair building, in a struct for neatness.
+typedef struct stairdata_s {
+    dfloat          stepDelta;
+    dint            direction;
+    dfloat          speed;
+    Material*       material;
+    dint            startDelay;
+    dint            startDelayDelta;
+    dint            textureChange;
+    dfloat          startHeight;
+} stairdata_t;
+#endif
+
+#if __JHERETIC__
+#define MAX_AMBIENT_SFX     8
+#endif
+
+#if __JDOOM__ || __JDOOM64__ || __JHERETIC__
+#define SP_floororigheight      planes[0].origHeight
+#define SP_ceilorigheight       planes[1].origHeight
+
+// Stair build flags.
+#define BL_BUILT            0x1
+#define BL_WAS_BUILT        0x2
+#define BL_SPREADED         0x4
+#endif
+
+typedef struct xsector_s {
+    dshort special;
+    dshort tag;  
+
+    /// 0 = untraversed, 1,2 = sndlines -1
+    dint soundTraversed;
+
+    /// Thing that made a sound (can be NULL).
+    Thing* soundTarget;
+
+    /// Stone, metal, heavy, etc...
+    dbyte seqType;
+
+    /// Thinker function for reversable actions.
+    void* specialData;
+
+    /// Original ID from the archived map format.
+    dint origID;
+
+#if __JDOOM__ || __JDOOM64__ || __JHERETIC__
+    /// Used during stair building.
+    dbyte blFlags;
+
+    struct {
+        dfloat origHeight;
+    } planes[2]; // {floor, ceiling}
+
+    dfloat origLight;
+    dfloat origRGB[3];
+
+    /// Extended generalized sectors.
+    xgsector_t* xg;
+#endif
+} xsector_t;
+
+typedef struct xlinedef_s {
+#if __JHEXEN__
+    dbyte special;
+    dbyte arg1;
+    dbyte arg2;
+    dbyte arg3;
+    dbyte arg4;
+    dbyte arg5;
+#else
+    dshort special;
+    dshort tag;
+#endif
+
+    short flags;
+
+    /// Has been rendered at least once and needs to appear in the map, for each player.
+    bool mapped[MAXPLAYERS];
+
+    dint validCount;
+
+    /// Original ID from the archived map format.
+    dint origID;
+
+#if __JDOOM64__
+    dshort useOn;
+#endif
+
+#if __JDOOM__ || __JDOOM64__ || __JHERETIC__
+    /// Extended generalized lines.
+    xgline_t* xg;
+#endif
+} xlinedef_t;
+
 /**
  * Base class for the game plugins' Map.
  */
 class LIBCOMMON_API GameMap : public de::Map
 {
 public:
-    GameMap();
+    /// Unique map name (e.g., MAP01, E1M1).
+    de::String name;
+
+    /// Logical episode number.
+    duint episode;
+
+    /// Logical map number.
+    duint map;
+
+    /// @c true = we are in the process of setting up this map.
+    bool inSetup;
+
+    dint time;
+    dint actualTime;
+
+    /// Game tic at map start, for time calculation.
+    dint startTic;
+
+    dint _numPlayerStarts;
+    playerstart_t* _playerStarts;
+
+    dint _numPlayerDMStarts;
+    playerstart_t* _deathmatchStarts;
+
+    duint _numSpawnSpots;
+    mapspot_t* _spawnSpots;
+
+#if __JHERETIC__
+    dint _maceSpotCount;
+    mapspot_t* _maceSpots;
+
+    dint _bossSpotCount;
+    mapspot_t* _bossSpots;
+#endif
+
+#if __JHERETIC__
+    dint const * ambientSfx[MAX_AMBIENT_SFX];
+    dint ambientSfxCount;
+    struct {
+        const dint* ptr;
+        dint tics;
+        dint volume;
+    } ambSfx;
+#endif
+
+#if __JHERETIC__ || __JHEXEN__
+    Thing lavaInflictor;
+#endif
+
+#if __JHEXEN__
+    dint _TIDList[MAX_TID_COUNT + 1]; // +1 for termination marker
+    Thing* _TIDMobj[MAX_TID_COUNT];
+#endif
+
+    xsector_t* _xSectors;
+    xlinedef_t* _xLineDefs;
+
+    /// For fast sight rejection.
+    dbyte* _rejectMatrix;
+
+    /// For crossed line specials.
+    iterlist_t* _spechit;
+
+    /// For surfaces that tick eg wall scrollers.
+    iterlist_t* _linespecials;
+
+    taglist_t* _lineTagLists;
+    dint numLineTagLists;
+
+    taglist_t* _sectorTagLists;
+    dint numSectorTagLists;
+
+    Thing* bodyQueue[BODYQUEUESIZE];
+    dint bodyQueueSlot;
+
+#if __JHEXEN__
+    Thing* corpseQueue[CORPSEQUEUESIZE];
+    dint corpseQueueSlot;
+#endif
+
+    struct spawnqueuenode_s* _spawnQueueHead;
+
+#if __JDOOM__ || __JDOOM64__
+    bool bossKilled;
+#endif
+
+    /// For intermission.
+    dint totalKills;
+    dint totalItems;
+    dint totalSecret;
+
+#if __JDOOM__
+    Thing* corpseHit;
+    Thing* vileObj;
+    Vector3f vileTry;
+
+    /// Global state of boss brain.
+    struct {
+        Thing** targets;
+        dint numTargets;
+        dint numTargetsAlloc;
+        dint easy;
+        dint targetOn;
+    } brain;
+#endif
+
+public:
+    GameMap(const de::String& name, duint episode, duint map);
     ~GameMap();
     
-    void load(const de::String& name);
+    void load(skillmode_t skill);
     
-    void operator << (de::Reader& from);    
+    void operator << (de::Reader& from);
+
+    /**
+     * Execute one game tic.
+     */
+    void runTick();
+
+#if __JHEXEN__
+    /**
+     * Initialize all polyobjs.
+     */
+    void initPolyobjs()
+#endif
+
+#if __JHERETIC__
+    void addMaceSpot(dfloat x, dfloat y, dangle angle);
+
+    void addBossSpot(dfloat x, dfloat y, dangle angle);
+#endif
+
+    void purgeDeferredSpawns();
+
+    /**
+     * Deferred Thing spawning until at least @minTics have passed.
+     * Spawn behavior is otherwise exactly the same as an immediate spawn.
+     */
+    void deferSpawnThing(dint minTics, mobjtype_t type,
+        const Vector3f& pos, dangle angle, sint spawnFlags,
+        void (*callback) (Thing* thing, void* paramaters) = 0, void* paramaters = 0);
+
+    void deferSpawnThing(dint minTics, mobjtype_t type,
+        dfloat x, dfloat y, dfloat z, dangle angle, dint spawnFlags,
+        void (*callback) (Thing* thing, void* paramaters) = 0, void* paramaters = 0) {
+            deferSpawnThing(minTics, type, Vector3f(x, y, z), angle, spawnFlags, callback, paramaters);
+    }
+
+    void precache();
+
+    void spawnSpecials();
+    void spawnPlayers();
+
+    void spawnPlayer(dint plrNum, playerclass_t pClass, dfloat x, dfloat y, dfloat z,
+        dangle angle, dint spawnFlags, bool makeCamera);
+
+    void spawnPlayerDM(dint playernum);
+
+    Thing* spawnThing(mobjtype_t type, dfloat x, dfloat y, dfloat z, dangle angle, dint spawnFlags);
+    Thing* spawnThing(mobjtype_t type, const dfloat pos[3], dangle angle, dint spawnFlags);
+
+    void updateSpecials();
+
+    void addPlayerStart(dint defaultPlrNum, duint entryPoint, bool deathmatch, dfloat x,
+        dfloat y, dfloat z, dangle angle, dint spawnFlags);
+
+    duint numPlayerStarts(bool deathmatch);
+    const playerstart_t* playerStart(duint entryPoint, dint pnum, bool deathmatch);
+
+    void clearPlayerStarts();
+    void dealPlayerStarts(duint entryPoint);
+
+#if __JHERETIC__
+    void addMaceSpot(dfloat x, dfloat y, dangle angle);
+    void addBossSpot(dfloat x, dfloat y, dangle angle);
+#endif
+
+#if __JDOOM64__
+    void thunder();
+#endif
+
+#if __JHEXEN__
+    void animateSurfaces();
+#endif
+
+    void destroyLineTagLists();
+    iterlist_t* iterListForTag(dint tag, bool createNewList);
+
+    void destroySectorTagLists();
+    iterlist_t* sectorIterListForTag(dint tag, bool createNewList);
+
+    xlinedef_t* XLineDef(duint idx);
+    xsector_t* XSector(duint idx);
+
+    dfloat gravity();
+
+    // @todo Should be private to GameMap.
+    iterlist_t* specHits();
+
+private:
+    /**
+     * Vars and structures bellow this point were formerly global. Their
+     * purpose varies but all are used by somewhat poorly implemented
+     * multi-part algorithms defining input, state and output. As such,
+     * they were moved into this class to prevent possible access and/or
+     * race issues should we wish to parallelize map AI/thinking.
+     *
+     * @todo Redesign the associated algorithms which reference these so
+     * their internal state is maintained "cleanly".
+     */
+#if __JDOOM__ || __JDOOM64__ || __JHERETIC__
+    dfloat dropoffDelta[2], floorZ;
+#endif
+
+#if __JHEXEN__
+    stairdata_t stairData;
+    stairqueue_t stairQueue[STAIR_QUEUE_SIZE];
+    dint stairQueueHead;
+    dint stairQueueTail;
+#endif
+
+    dfloat tmBBox[4];
+    Thing* tmThing;
+
+    /// If @c true, move would be ok if within "tmFloorZ - tmCeilingZ".
+    bool floatOk;
+
+    dfloat tmFloorZ;
+    dfloat tmCeilingZ;
+#if __JHEXEN__
+    Material* tmFloorMaterial;
+#endif
+
+    bool fellDown; // $dropoff_fix
+
+    /**
+     * The following are used to keep track of the linedefs that clip the open
+     * height range e.g. PIT_CheckLine. They in turn are used with the &unstuck
+     * logic and to prevent missiles from exploding against sky hack walls.
+     */
+    LineDef* ceilingLine;
+    LineDef* floorLine;
+
+    /// Who got hit (or NULL).
+    Thing* lineTarget;
+
+    /// $unstuck: blocking linedef.
+    LineDef* blockLine;
+
+    dfloat attackRange;
+
+#if __JHEXEN__
+    Thing* puffSpawned;
+    Thing* blockingThing;
+#endif
+#if __JHERETIC__ || __JHEXEN__
+    mobjtype_t puffType;
+    Thing* missileThing;
+#endif
+
+    Vector3f tm;
+#if __JDOOM__ || __JDOOM64__ || __JHERETIC__
+    dfloat tmHeight;
+    LineDef* tmHitLine;
+#endif
+    dfloat tmDropoffZ;
+    dfloat bestSlideFrac, secondSlideFrac;
+    LineDef* bestSlideLine, *secondSlideLine;
+
+    Thing* slideThing;
+
+    Vector3f tmMove;
+    Thing* shootThing;
+
+    /// Height if not aiming up or down.
+    sfloat shootZ;
+
+    dint lineAttackDamage;
+    dfloat aimSlope;
+
+    /// Slopes to top and bottom of target
+    dfloat topSlope, bottomSlope;
+
+    Thing* useThing;
+
+    Thing* bombSource, *bombSpot;
+    dint bombDamage;
+    dint bombDistance;
+
+    bool crushChange;
+    bool noFit;
+
+    /// Start position for trajectory line checks.
+    Vector3f startPos;
+
+    /// End position for trajectory checks.
+    Vector3f endPos;
+
+#if __JHEXEN__
+    Thing* tsThing;
+    bool damageSource;
+
+    /// Generic global onThing...used for landing on pods/players.
+    Thing* onThing;
+
+    Thing* puzzleItemUser;
+    dint puzzleItemType;
+    bool puzzleActivated;
+#endif
+
+#if !__JHEXEN__
+    /// $unstuck: used to check unsticking.
+    dint tmUnstuck;
+#endif
 };
 
 #endif /* LIBCOMMON_GAMEMAP_H */
--- doomsday/libcommon/src/gamemap.cc
+++ doomsday/libcommon/src/gamemap.cc
@@ -1,7 +1,8 @@
 /*
  * The Doomsday Engine Project
  *
- * Copyright (c) 2009 Jaakko Keränen <jaakko.keranen@iki.fi>
+ * Copyright © 2003-2010 Jaakko Keränen <jaakko.keranen@iki.fi>
+ * Copyright © 2005-2010 Daniel Swanson <danij@dengine.net>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -29,13 +30,186 @@ extern "C" {
 
 using namespace de;
 
-GameMap::GameMap()
+namespace {
+typedef struct spawnqueuenode_s {
+    dint            startTime;
+    dint            minTics; // Minimum number of tics before respawn.
+    void          (*callback) (Object* object, void* context);
+    void*           context;
+
+    dfloat          pos[3];
+    dangle          angle;
+    mobjtype_t      type;
+    dint            spawnFlags; // MSF_* flags
+
+    struct spawnqueuenode_s* next;
+} spawnqueuenode_t;
+
+spawnqueuenode_t* allocateNode(void)
+{
+    return Z_Malloc(sizeof(spawnqueuenode_t), PU_STATIC, 0);
+}
+
+void freeNode(map_t* map, spawnqueuenode_t* node)
+{
+    // Find this node in the spawn queue and unlink it if found.
+    if(map->_spawnQueueHead)
+    {
+        if(map->_spawnQueueHead == node)
+        {
+            map->_spawnQueueHead = map->_spawnQueueHead->next;
+        }
+        else
+        {
+            spawnqueuenode_t* n;
+
+            for(n = map->_spawnQueueHead; n->next; n = n->next)
+            {
+                if(n->next == node)
+                    n->next = n->next->next;
+            }
+        }
+    }
+
+    Z_Free(node);
+}
+
+spawnqueuenode_t* dequeueSpawn(map_t* map)
+{
+    spawnqueuenode_t* n = map->_spawnQueueHead;
+
+    if(map->_spawnQueueHead)
+        map->_spawnQueueHead = map->_spawnQueueHead->next;
+
+    return n;
+}
+
+void emptySpawnQueue(map_t* map)
+{
+    if(map->_spawnQueueHead)
+    {
+        spawnqueuenode_t* n;
+
+        while((n = dequeueSpawn(map)))
+            freeNode(map, n);
+    }
+
+    map->_spawnQueueHead = NULL;
+}
+
+void enqueueSpawn(map_t* map, int minTics, mobjtype_t type,
+                         float x, float y, float z, angle_t angle, int spawnFlags,
+                         void (*callback) (mobj_t* mo, void* context),
+                         void* context)
+{
+    spawnqueuenode_t* n = allocateNode();
+
+    n->type = type;
+    n->pos[VX] = x;
+    n->pos[VY] = y;
+    n->pos[VZ] = z;
+    n->angle = angle;
+    n->spawnFlags = spawnFlags;
+
+    n->startTime = map->time;
+    n->minTics = minTics;
+
+    n->callback = callback;
+    n->context = context;
+
+    if(map->_spawnQueueHead)
+    {   // Find the correct insertion point.
+        if(map->_spawnQueueHead->next)
+        {
+            spawnqueuenode_t* l = map->_spawnQueueHead;
+
+            while(l->next &&
+                  l->next->minTics - (map->time - l->next->startTime) <= minTics)
+                l = l->next;
+
+            n->next = (l->next? l->next : NULL);
+            l->next = n;
+        }
+        else
+        {   // After or before the head?
+            if(map->_spawnQueueHead->minTics -
+               (map->time - map->_spawnQueueHead->startTime) <= minTics)
+            {
+                n->next = NULL;
+                map->_spawnQueueHead->next = n;
+            }
+            else
+            {
+                n->next = map->_spawnQueueHead;
+                map->_spawnQueueHead = n;
+            }
+        }
+    }
+    else
+    {
+        n->next = NULL;
+        map->_spawnQueueHead = n;
+    }
+}
+
+mobj_t* doDeferredSpawn(map_t* map)
+{
+    mobj_t* mo = NULL;
+
+    // Anything due to spawn?
+    if(map->_spawnQueueHead &&
+       map->time - map->_spawnQueueHead->startTime >= map->_spawnQueueHead->minTics)
+    {
+        spawnqueuenode_t* n = dequeueSpawn(map);
+
+        // Spawn it.
+        if((mo = GameMap_SpawnMobj3fv(map, n->type, n->pos, n->angle, n->spawnFlags)))
+        {
+            if(n->callback)
+                n->callback(mo, n->context);
+        }
+
+        freeNode(map, n);
+    }
+
+    return mo;
+}
+
+/**
+ * Called 35 times per second by P_DoTick.
+ */
+void doDeferredSpawns(map_t* map)
+{
+    while(doDeferredSpawn(map));
+}
+}
+
+GameMap::GameMap(const de::String& name, duint episode, duint map)
+ : name(name), episode(episode), map(map)
 {}
 
 GameMap::~GameMap()
-{}
+{
+    if(_xLineDefs)
+        Z_Free(_xLineDefs); _xLineDefs = NULL;
 
-void GameMap::load(const de::String& name)
+    if(_xSectors)
+        Z_Free(_xSectors); _xSectors = NULL;
+
+    if(_spechit)
+        P_DestroyIterList(_spechit); _spechit = NULL;
+
+    if(_linespecials)
+        P_DestroyIterList(_linespecials); _linespecials = NULL;
+
+    if(_rejectMatrix)
+        Z_Free(_rejectMatrix); _rejectMatrix = NULL;
+
+    destroyLineTagLists();
+    destroySectorTagLists();
+}
+
+void GameMap::load()
 {
     Map::load(name);
 }
@@ -55,3 +229,128 @@ void GameMap::operator << (Reader& from)
     }
     */
 }
+
+void GameMap::runTick()
+{
+    actualTime++;
+
+    // Pause if in menu and at least one tic has been run.
+    if(!IS_NETGAME && (Hu_MenuIsActive() || Hu_IsMessageActive()) &&
+       !Get(DD_PLAYBACK) && map->time > 1)
+        return;
+
+    runThinkers();
+
+    updateSpecials();
+    doDeferredSpawns(this);
+
+#if __JDOOM64__
+    thunder();
+#endif
+#if __JHERETIC__
+    playAmbientSfx();
+#endif
+#if __JHEXEN__
+    animateSurfaces();
+#endif
+
+    // For par times, among other things.
+    time++;
+}
+
+#if __JHEXEN__
+void GameMap::initPolyobjs()
+{
+    LOG_MESSAGE("GameMap::initPolyobjs: Initializing polyobjects.");
+
+    for(duint i = 0; i < numPolyobjs(); ++i)
+    {
+        const mapspot_t* spot;
+        Polyobj* po;
+        duint j;
+        
+        po = polyobj(i | 0x80000000);
+
+        // Init game-specific properties.
+        po->specialData = NULL;
+
+        // Find the mapspot associated with this polyobj.
+        j = 0;
+        spot = NULL;
+        while(j < _numSpawnSpots && !spot)
+        {
+            if((_spawnSpots[j].doomEdNum == PO_SPAWN_DOOMEDNUM ||
+                _spawnSpots[j].doomEdNum == PO_SPAWNCRUSH_DOOMEDNUM) &&
+               _spawnSpots[j].angle == po->tag)
+            {   // Polyobj mapspot.
+                spot = &_spawnSpots[j];
+            }
+            else
+            {
+                j++;
+            }
+        }
+
+        if(spot)
+        {
+            po->crush = (spot->doomEdNum == PO_SPAWNCRUSH_DOOMEDNUM? 1 : 0);
+            po->translate(-po->origin + spot->pos);
+        }
+        else
+        {
+            LOG_WARNING("GameMap::initPolyobjs: Warning, missing mapspot for polyobj #")
+                << i << ".";
+        }
+    }
+}
+#endif
+
+void GameMap::deferSpawnThing(dint minTics, mobjtype_t type,
+    const Vector3f& pos, dangle angle, sint spawnFlags,
+    void (*callback) (Object* object, void* paramaters), void* paramaters)
+{
+    if(minTics > 0)
+    {
+        enqueueSpawn(this, minTics, type, pos.x, pos.y, pos.z, angle,
+                     spawnFlags, callback, paramaters);
+    }
+    else // Spawn immediately.
+    {
+        Thing* thing;
+
+        if((thing = spawnThing(type, pos, angle, spawnFlags)))
+        {
+            if(callback)
+                callback(thing, paramaters);
+        }
+    }
+}
+
+void GameMap::purgeDeferredSpawns()
+{
+    emptySpawnQueue(this);
+}
+
+#if __JHERETIC__
+void GameMap::addMaceSpot(dfloat x, dfloat y, dangle angle)
+{
+    mapspot_t* spot;
+
+    _maceSpots = Z_Realloc(_maceSpots, sizeof(mapspot_t) * ++_maceSpotCount, PU_MAP);
+    spot = &_maceSpots[_maceSpotCount-1];
+
+    spot->pos = Vector2f(x, y);
+    spot->angle = angle;
+}
+
+void GameMap::addBossSpot(dfloat x, dfloat y, dangle angle)
+{
+    mapspot_t* spot;
+
+    _bossSpots = Z_Realloc(_bossSpots, sizeof(mapspot_t) * ++_bossSpotCount, PU_MAP);
+    spot = &_bossSpots[_bossSpotCount-1];
+
+    spot->pos = Vector2f(x, y);
+    spot->angle = angle;
+}
+#endif
