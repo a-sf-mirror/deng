16b1e44885995ca2e93e28d2b359ea42df80dd78
danij <danij@dengine.net>
2011-10-15 14:42:36 +0100

    Embed localized transforms into The One installer msi
    Use the localized versions of the Doomsday Windows Installer to generate
    a transform which is then embedded into the main .msi Now when executing
    the .msi a localized version of the UI can be chosen with the argument:
    
      TRANSFORMS":<ISO 639-1>-<ISO 3166-1>"
    
    Modified the localization construction so that strings defined for the
    en-us locale are used as fallbacks regardless of the current locale.
    
    Make use of the finished default translations for the prebuilt WixUI_Mondo
    UI to embed Danish, French and Romanian localizations in the final .msi
    
    Todo: There is presently a codepage mismatch issue with the Chinese and
    Slovakian localizations that prevents them from being used.

--- distrib/platform_buildinstaller.py
+++ distrib/platform_buildinstaller.py
@@ -9,9 +9,13 @@ import sys
 import os
 import platform
 import shutil
+import subprocess
 import time
 import build_version
 import build_number
+
+WIX_DIR = os.getenv('WIX_DIR')
+WINDOWS_SDK_DIR = os.getenv('WINDOWS_SDK_DIR')
 
 LAUNCH_DIR = os.path.abspath(os.getcwd())
 SOURCE_DIR = os.path.join(LAUNCH_DIR, 'win32\\')
@@ -30,21 +34,27 @@ DOOMSDAY_BUILD_NUMBER = build_number.todays_build()
 DOOMSDAY_BUILD = 'build' + DOOMSDAY_BUILD_NUMBER
 TIMESTAMP = time.strftime('%y-%m-%d')
 
+verbose = False
+quiet = False
+
 
 def exit_with_error():
     os.chdir(LAUNCH_DIR)
     sys.exit(1)
 
+
 def mkdir(n):
     try:
         os.mkdir(n)
     except OSError:
-        print 'Directory', n, 'already exists.'
+        if not quiet:
+            print 'Directory', n, 'already exists.'
 
 
 def remkdir(n):
     if os.path.exists(n):
-        print n, 'exists, clearing it...'
+        if not quiet:
+            print n, 'exists, clearing it...'
         shutil.rmtree(n, True)
     os.mkdir(n)
 
@@ -69,7 +79,8 @@ def find_version():
 
 def prepare_work_dir():
     remkdir(WORK_DIR)
-    print "Work directory prepared."
+    if not quiet:
+        print "Work directory prepared."
 
 
 def clean_work_dir():
@@ -86,25 +97,27 @@ def compile_wix_sources(files, locale=None):
     if not (files and isinstance(files, list)):
        raise Exception("compile_wix_sources: Expected a list for Arg0 but received something else.")
 
-    print 'Compiling WiX source files:'
-    sys.stdout.flush()
+    CDEFS = ['-dProductVersionMajor=' + str(DOOMSDAY_VERSION_MAJOR),
+             '-dProductVersionMinor=' + str(DOOMSDAY_VERSION_MINOR),
+             '-dProductVersionRevision=' + str(DOOMSDAY_VERSION_REVISION),
+             '-dProductBuildNumber=' + str(DOOMSDAY_BUILD_NUMBER)]
 
-    CDEFS = '-dProductVersionMajor="' + str(DOOMSDAY_VERSION_MAJOR) + '"' \
-         + ' -dProductVersionMinor="' + str(DOOMSDAY_VERSION_MINOR) + '"' \
-         + ' -dProductVersionRevision="' + str(DOOMSDAY_VERSION_REVISION) + '"' \
-         + ' -dProductBuildNumber="' + str(DOOMSDAY_BUILD_NUMBER) + '"'
     if locale and isinstance(locale, dict):
-        CDEFS += " -dLanguage=%i" % locale['language']
-        CDEFS += " -dCodepage=%i" % locale['codepage']
+        CDEFS += ['-dLanguage=' + "%i" % locale['language'],
+                  '-dCodepage=' + "%i" % locale['codepage']]
     else:
         # Assume en-us
-        CDEFS += " -dLanguage=1033"
-        CDEFS += " -dCodepage=1252"
+        CDEFS += ['-dLanguage=1033', '-dCodepage=1252']
+
+    if not quiet:
+        print 'Compiling WiX source files' + ('...', ':')[verbose != 0]
+    sys.stdout.flush()
 
     # Compile sources.
     for file in files:
         # Compose source file name.
-        print "  %s..." % os.path.normpath(file['source'] + '.wxs')
+        if not quiet and verbose:
+            print "  %s..." % os.path.normpath(file['source'] + '.wxs')
         srcFile = os.path.join(SOURCE_DIR, file['source'] + '.wxs')
 
         # Compose out file name
@@ -114,59 +127,108 @@ def compile_wix_sources(files, locale=None):
         outFile += '.wixobj'
 
         # Compile.
-        if os.system('candle -nologo ' + CDEFS + ' -out ' + outFile + ' -wx0099 ' + srcFile):
+        cmd = ['candle', '-nologo', '-out', "%s"%outFile, '-wx0099', "%s"%srcFile]
+        if subprocess.call(cmd + CDEFS, cwd=WIX_DIR):
             raise Exception("Failed compiling %s" % srcFile)
 
 
 """ Linking of WiX source files and installable binding """
-def link_wix_objfiles(outFileName, files, locale=None):
+def link_wix_objfiles(outDir, outFileName, files, locale=None):
     if not (files and isinstance(files, list)):
-       raise Exception("link_wix_objfiles: Expected a list for Arg1 but received something else.")
+       raise Exception("link_wix_objfiles: Expected a list for Arg2 but received something else.")
 
     # Compose the full path to the target file.
-    target = os.path.join(OUTPUT_DIR, outFileName + '.msi')
+    target = os.path.join(outDir, outFileName + '.msi')
     try:
         os.remove(target)
-        print 'Removed existing target file', target
+        if not quiet:
+            print 'Removed existing target file', target
     except:
-        print 'Target:', target
+        if not quiet:
+            print 'Target:', target
 
-    print 'Linking WiX object files:'
+    if not quiet:
+        print 'Linking WiX object files' + ('...', ':')[verbose != 0]
 
-    # Compose the link object file list.
-    objFileList = ' ';
+    # Compose the link file command list.
+    LCMDS = []
     for file in files:
         # Compose link object file name.
-        fileWithExt = file['source']
+        fileName = file['source']
         if locale and isinstance(locale, dict):
-            fileWithExt += '_' + locale['culture']
-        fileWithExt += '.wixobj'
+            fileName += '_' + locale['culture']
 
-        print "  %s..." % os.path.normpath(fileWithExt)
-        objFileList += os.path.join(WORK_DIR, fileWithExt) + ' '
+        LCMDS += [os.path.join(WORK_DIR, fileName + '.wixobj')]
+
+        if verbose:
+            sys.stdout.write("  " + os.path.normpath(fileName + '.wixobj'))
+
+        # Is there a base/default localization file to go with this?
+        if 'defLocale' in file:
+            LCMDS += ['-loc', os.path.join(SOURCE_DIR, file['defLocale'])]
+            if verbose:
+                sys.stdout.write(' loc:' + os.path.normpath(file['defLocale']))
 
         # Is there a localization file to go with this?
         if 'locale' in file:
-            objFileList += '-loc ' + file['locale'] + ' '
+            LCMDS += ['-loc', os.path.join(SOURCE_DIR, file['locale'])]
+            if verbose:
+                sys.stdout.write(' loc:' + os.path.normpath(file['locale']))
+
+        if verbose:
+            print ""
 
     # Compose link arguments.
-    LARGS = '-nologo -b ' + PRODUCTS_DIR \
-         + ' -out ' + target \
-         + ' -pdbout ' + os.path.join(WORK_DIR, outFileName + '.wixpdb') \
-         + ' -ext WixUIExtension -ext WixUtilExtension'
+    cmd = ['light', '-nologo', '-b', PRODUCTS_DIR,
+           '-out', target,
+           '-pdbout', os.path.join(WORK_DIR, outFileName + '.wixpdb'),
+           '-ext', 'WixUIExtension', '-ext', 'WixUtilExtension']
 
     if locale and isinstance(locale, dict):
-        LARGS += ' -cultures:' + locale['culture']
+        cmd += ['-cultures:%s;en-us' % locale['culture']]
     else:
-        LARGS += ' -cultures:' + 'en-us'
+        cmd += ['-cultures:en-us']
 
     # Link all objects and bind our installables into cabinents.
     sys.stdout.flush()
-    if os.system('light ' + LARGS + objFileList):
+    if subprocess.call(cmd + LCMDS, cwd=WIX_DIR):
         raise Exception("Failed linking WiX object files.")
 
 
+def build_file_list(files, defCulture, localeCulture=None):
+    if not (files and isinstance(files, list)):
+       raise Exception("build_file_list: Expected a list for Arg0 but received something else.")
+
+    fl = []
+    # Compose the file list for this locale.
+    for f in files:
+        locd = dict(source = f)
+
+        # Is there a base/default localization for this file?
+        locf = os.path.join(defCulture, f + '.wxl')
+        if os.path.exists(os.path.join(SOURCE_DIR, locf)):
+            locd['defLocale'] = locf
+
+        # Is there a localization for this file?
+        if localeCulture:
+            locf = os.path.join(localeCulture, f + '.wxl')
+            if os.path.exists(os.path.join(SOURCE_DIR, locf)):
+                locd['locale'] = locf
+
+        # Add this to the list of files for this locale
+        fl.append(locd)
+    return fl
+
+
 def win_installer():
+    if not os.path.exists(WIX_DIR):
+        raise Exception("Failed to locate WiX toolset. Check WIX_DIR is set and correct.\nWIX_DIR=%s" % WIX_DIR)
+
+    # Compose the path to the substorage script.
+    subStoreScript = os.path.join(WINDOWS_SDK_DIR, 'Samples', 'sysmgmt', 'msi', 'scripts', 'WiSubStg.vbs')
+    if not os.path.exists(subStoreScript):
+        raise Exception("Failed to locate WiSubStg.vbs script in the Windows SDK. Check WINDOWS_SDK_DIR is set and correct.\nWINDOWS_SDK_DIR=%s" % WINDOWS_SDK_DIR)
+
     # Compose outfile name
     global DOOMSDAY_VERSION_MAJOR
     global DOOMSDAY_VERSION_MINOR
@@ -192,41 +254,63 @@ def win_installer():
 
     prepare_work_dir()
 
-    # Compile all locale versions.
-    locales = [dict(culture = 'en-us', codepage = 1252, language = 1033, base = True)]
+    # Compile the base language installer.
+    if not quiet:
+        print 'Compiling base-/default- language installer...'
 
+    base = dict(culture = 'en-us', codepage = 1252, language = 1033)
+    fl = build_file_list(files, 'en-us')
+    compile_wix_sources(fl, base)
+    link_wix_objfiles(OUTPUT_DIR, OUTFILE, fl, base)
+
+    # Compile all localized versions.
+    locales = [dict(culture = 'da-dk', codepage = 1252, language = 1030),
+               dict(culture = 'fr-fr', codepage = 1252, language = 1036),
+               dict(culture = 'ro-ro', codepage = 1250, language = 1048)]
     for l in locales:
-        fl = []
-        # Compose the file list for this locale.
-        for f in files:
-            locd = dict(source = f)
-
-            # Is there a localization for this file?
-            locf = os.path.join(SOURCE_DIR, l['culture'], f + '.wxl')
-            if os.path.exists(locf):
-                locd['locale'] = locf
-
-            # Add this to the list of files for this locale
-            fl.append(locd)
-
-        if 'base' in l and l['base'] == True:
-            print 'Compiling default-/base- language installer...'
-            outFile = OUTFILE
-        else:
+        if not quiet:
             print 'Compiling %s localized installer...' % l['culture']
-            outFile = OUTFILE + '_' + l['culture']
 
+        fl = build_file_list(files, 'en-us', l['culture'])
         compile_wix_sources(fl, l)
-        link_wix_objfiles(outFile, fl, l)
+        link_wix_objfiles(WORK_DIR, OUTFILE + '_' + l['culture'], fl, l)
+
+        # Construct the locale transform.
+        if not quiet:
+            print 'Constructing ms transform...'
+
+        baseMSI = os.path.join(OUTPUT_DIR, OUTFILE + '.msi')
+        localeMSI = os.path.join(WORK_DIR, OUTFILE + '_' + l['culture'] + '.msi')
+        outFile   = os.path.join(WORK_DIR, OUTFILE + '_' + l['culture'] + '.mst')
+
+        cmd = ['torch', '-nologo', '-t', 'language', baseMSI, localeMSI, '-out', outFile]
+        if subprocess.call(cmd, cwd=WIX_DIR):
+            raise Exception("Failed building ms transform for %s" % localeMSI)
+
+        # Merge the transform into the base installer.
+        if not quiet:
+            print 'Merging transform into base-/default- language installer...'
+
+        cmd = ['cscript', '/nologo', subStoreScript, baseMSI, outFile, str(l['culture'])]
+        if subprocess.call(cmd):
+            raise Exception("Failed merging ms transform %s into %s" % (outFile, baseMSI))
 
     # Cleanup
     clean_work_dir()
 
 
 def main():
+    global quiet
+    global verbose
+
     find_version()
 
-    print "Checking OS...",
+    quiet = '-quiet' in sys.argv
+    if not quiet:
+        verbose = '-verbose' in sys.argv
+
+    if not quiet:
+        print "Checking OS...",
 
     try:
         if sys.platform != "win32":
@@ -234,7 +318,8 @@ def main():
             print "I don't know how to make an installer on this platform."
             exit_with_error()
 
-        print "Windows"
+        if not quiet:
+            print "Windows"
         win_installer()
     except Exception, x:
         print "Errors encountered building Doomsday Engine Installer."
